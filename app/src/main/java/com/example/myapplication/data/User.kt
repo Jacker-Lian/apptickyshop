05bc87c72f8d8763f50b303135e9ca308b437442
Estudiante <luis9220chino@gmail.com>
2025-09-03 19:40:02 -0500

    solo falta   lo del nombre

--- app/src/main/java/com/example/myapplication/data/User.kt
+++ app/src/main/java/com/example/myapplication/data/User.kt
@@ -1,8 +1,10 @@
 package com.example.myapplication.data
-
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 @Entity(tableName = "users")
 data class User(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
     val email: String,
     val password: String
+
 )
--- app/src/main/java/com/example/myapplication/data/UserDao.kt
+++ app/src/main/java/com/example/myapplication/data/UserDao.kt
@@ -1,5 +1,8 @@
 package com.example.myapplication.data
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.Query
 @Dao
 interface UserDao {
     @Insert
